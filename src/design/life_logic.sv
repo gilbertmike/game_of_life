29c5233c8ca9da47d63cd27c80726b0e5a14ec9b
Michael Gilbert <gilbertm@mit.edu>
2021-10-21 23:13:44 -0400

    Synchronizer

--- src/design/life_logic.sv
+++ src/design/life_logic.sv
@@ -6,6 +6,7 @@ module life_logic#(parameter ADDR_SIZE = 32,
                   (input wire clk_in,
                    input wire[LOG_MAX_SPEED-1:0] speed_in,
                    input wire[LINE_WIDTH-1:0] data_in,
+                   output logic done_out,
                    output logic[ADDR_SIZE-1:0] addr_r_out,
                    output logic[ADDR_SIZE-1:0] addr_w_out,
                    output logic[LINE_WIDTH-1:0] data_out);
--- src/design/top_level.sv
+++ src/design/top_level.sv
@@ -16,10 +16,34 @@ module top_level(input wire clk_100mhz,
     logic[LINE_WIDTH-1:0] data_logic_w;
     logic[LINE_WIDTH-1:0] data_logic_r;
     
+    logic buf_swap, logic_done;
+    logic[1:0] done_count;
+    always_ff @(posedge clk_100mhz) begin
+        if (sw[15]) begin
+            done_count <= 2'd0;
+        end else if (done_count == 2'd0) begin
+            buf_swap <= 1'b0;
+        end else begin
+            case ({logic_done, vga_vs})
+                2'b11: begin
+                    done_count <= 2'd0;
+                    buf_swap <= 1'b1;
+                end
+                2'b10: done_count <= 2'd1;
+                2'b01: done_count <= 2'd1;
+                default: begin
+                    done_count <= 2'd0;
+                    buf_swap <= 1'b0;
+                end
+            endcase
+        end
+    end
+
     double_buffer#(.ADDR_SIZE(ADDR_SIZE), .LINE_WIDTH(LINE_WIDTH)) db(
-        .addr_rend_r(addr_rend_r), .addr_logic_r(addr_logic_r),
-        .addr_logic_w(addr_logic_w), .data_rend_r(data_rend_r),
-        .data_logic_w(data_logic_w), .data_logic_r(data_logic_r));
+        .clk_in(clk_100mhz), .addr_rend_r(addr_rend_r), .swap_in(buf_swap),
+        .addr_logic_r(addr_logic_r), .addr_logic_w(addr_logic_w),
+        .data_rend_r(data_rend_r), .data_logic_w(data_logic_w),
+        .data_logic_r(data_logic_r));
 
     renderer#(.LINE_WIDTH(LINE_WIDTH)) renderer0(
         .clk_in(clk_100mhz), .data_in(data_rend_r), .addr_r_out(addr_rend_r),
@@ -31,6 +55,6 @@ module top_level(input wire clk_100mhz,
         life_logic0(.clk_in(clk_100mhz),
                     .speed_in(sw[SPEED_SW+LOG_MAX_SPEED-1:SPEED_SW]),
                     .addr_r_out(addr_logic_r), .addr_w_out(addr_logic_w),
-                    .data_out(data_logic_w));
+                    .data_out(data_logic_w), .done_out(logic_done));
 endmodule
 `default_nettype wire
