4963b120835f56a869f3efd305f5d54c0d50b160
Michael Gilbert <gilbertm@mit.edu>
2021-11-01 22:03:27 -0400

    Test double_buffer without pre-made pattern

--- src/design/double_buffer.sv
+++ src/design/double_buffer.sv
@@ -1,6 +1,9 @@
+`include "common.svh"
+
 `default_nettype none
 module double_buffer#(ADDR_SIZE=32, LINE_WIDTH=8)
                      (input wire clk_in,
+                      input wire rst_in,
                       input wire swap_in,
                       input wire[ADDR_SIZE-1:0] render_addr_r,
                       input wire[ADDR_SIZE-1:0] logic_addr_r,
@@ -9,55 +12,58 @@ module double_buffer#(ADDR_SIZE=32, LINE_WIDTH=8)
                       input wire logic_wr_en,
                       output logic[LINE_WIDTH-1:0] render_data_r,
                       output logic[LINE_WIDTH-1:0] logic_data_r);
-    addr_t buf0_addr_r;
-    addr_t buf0_addr_w;
-    data_t buf0_data_r;
-    data_t buf0_data_w;
-    logic buf0_wr_en;
-    bram_buffer0 buf0(.clka(clk_in), .addra(buf0_addr_w), .dina(buf0_data_w),
-                      .wea(buf0_wr_en), .clkb(clk_in), .addrb(buf0_addr_r), 
-                      .doutb(buf0_data_r));
+    addr_t buf0_addra;
+    data_t buf0_data_ra;
+    data_t buf0_data_wa;
+    logic buf0_wr_ena;
+    addr_t buf0_addrb;
+    data_t buf0_data_rb;
+    bram_buffer0 buf0(.clka(clk_in), .addra(buf0_addra), .dina(buf0_data_wa),
+                      .douta(buf0_data_ra), .wea(buf0_wr_ena),
+                      .clkb(clk_in), .addrb(buf0_addrb), .dinb(0),
+                      .doutb(buf0_data_rb), .web(0));
 
-    addr_t buf1_addr_r;
-    addr_t buf1_addr_w;
-    data_t buf1_data_r;
-    data_t buf1_data_w;
-    logic buf1_wr_en;
-    bram_buffer1 buf1(.clka(clk_in), .addra(buf1_addr_w), .dina(buf1_data_w),
-                      .wea(buf1_wr_en), .clkb(clk_in), .addrb(buf1_addr_r), 
-                      .doutb(buf1_data_r));
+    addr_t buf1_addra;
+    data_t buf1_data_ra;
+    data_t buf1_data_wa;
+    logic buf1_wr_ena;
+    addr_t buf1_addrb;
+    data_t buf1_data_rb;
+    bram_buffer1 buf1(.clka(clk_in), .addra(buf1_addra), .dina(buf1_data_wa),
+                      .douta(buf1_data_ra), .wea(buf1_wr_ena),
+                      .clkb(clk_in), .addrb(buf1_addrb), .dinb(0),
+                      .doutb(buf1_data_rb), .web(0));
 
-    // buffer_toggle decides if logic goes to buf0 or buf1
+    // buffer_toggle decides if logic writes to buf0 or buf1
     logic buffer_toggle;
     always_ff @(posedge clk_in) begin
-        buffer_toggle <= swap_in ? !buffer_toggle : buffer_toggle;
+        if (rst_in)
+            buffer_toggle <= 0;
+        else
+            buffer_toggle <= swap_in ? !buffer_toggle : buffer_toggle;
     end
 
     always_comb begin
         if (buffer_toggle) begin
-            buf0_addr_r = logic_addr_r;
-            logic_data_r = buf0_data_r;
-            buf0_addr_w = logic_addr_w;
-            buf0_data_w = logic_data_w;
-            buf0_wr_en = logic_wr_en;
+            buf0_addra = logic_addr_w;
+            buf0_data_wa = logic_data_w;
+            buf0_wr_ena = logic_wr_en;
 
-            buf1_addr_r = render_addr_r;
-            render_data_r = buf1_data_r;
-            buf1_addr_w = 0;
-            buf1_data_w = 0;
-            buf1_wr_en = 0;
+            buf1_addra = logic_addr_r;
+            logic_data_r = buf1_data_ra;
+            buf1_addrb = render_addr_r;
+            render_data_r = buf1_data_rb;
+            buf1_wr_ena = 0;
         end else begin
-            buf0_addr_r = render_addr_r;
-            render_data_r = buf0_data_r;
-            buf0_addr_w = 0;
-            buf0_data_w = 0;
-            buf0_wr_en = 0;
+            buf1_addra = logic_addr_w;
+            buf1_data_wa = logic_data_w;
+            buf1_wr_ena = logic_wr_en;
 
-            buf1_addr_r = logic_addr_r;
-            logic_data_r = buf1_data_r;
-            buf1_addr_w = logic_addr_w;
-            buf1_data_w = logic_data_w;
-            buf1_wr_en = logic_wr_en;
+            buf0_addra = logic_addr_r;
+            logic_data_r = buf0_data_ra;
+            buf0_addrb = render_addr_r;
+            render_data_r = buf0_data_rb;
+            buf0_wr_ena = 0;
         end
     end
 endmodule
---
+++ src/sim/double_buffer_tb.sv
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+
+`include "common.svh"
+
+module double_buffer_tb;
+    logic clk;
+    always #5 clk = !clk;
+
+    // Input
+    addr_t logic_addr_r;
+    addr_t logic_addr_w;
+    data_t logic_data_w;
+    logic logic_wr_en;
+    addr_t render_addr_r;
+
+    logic swap;
+    logic rst;
+    
+    // Output
+    data_t logic_data_r;
+    data_t render_data_r;
+
+    double_buffer db(.logic_addr_r(logic_addr_r), .logic_addr_w(logic_addr_w),
+                     .logic_data_w(logic_data_w), .logic_wr_en(logic_wr_en),
+                     .render_addr_r(render_addr_r), .swap_in(swap),
+                     .rst_in(rst), .clk_in(clk), .logic_data_r(logic_data_r),
+                     .render_data_r(render_data_r));
+
+    initial begin
+        #10;
+        clk = 0;
+        rst = 1;
+        swap = 0;
+        #50;
+        rst = 0;
+
+        // Logic stores data in address 0
+        #10;
+        logic_addr_w = 0;
+        logic_data_w = 1;
+        logic_wr_en = 1;
+
+        logic_addr_r = 0;
+        render_addr_r = 0;
+        #10;
+        logic_wr_en = 0;
+
+        // Swap. Render and logic should be able to see the data
+        #10;
+        swap = 1;
+        #10;
+        swap = 0;
+        render_addr_r = 0;
+        logic_addr_r = 0;
+        #10;
+        assert(logic_data_r == 1);
+        assert(render_data_r == 1);
+    end
+endmodule
