4f9613b704ac97fce299cb0297a796d227d9373e
FionaLin13 <59299503+FionaLin13@users.noreply.github.com>
2021-11-02 21:16:04 -0400

    start synchronizer_tb

---
+++ src/sim/synchronizer_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/02/2021 09:14:03 PM
+// Design Name: 
+// Module Name: synchronizer_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module synchronizer_tb(
+
+    );
+endmodule
