75c13b539b8766e107853861f6dd90706c82f091
Michael Gilbert <mikegilbert1902@gmail.com>
2021-11-13 16:18:35 -0500

    Add game logic testbenches (#20)
    * Rename LINE_WIDTH to WORD_SIZE
    
    * Missing semicolon fix
    
    * Implement (untested) fetcher
    
    * Implement (untested) life_logic modules
    
    * Add test benches for game logic
    
    Co-authored-by: Michael Gilbert <gilbertm@mit.edu>

---
+++ src/sim/logic_writeback_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/13/2021 04:14:21 PM
+// Design Name: 
+// Module Name: logic_writeback_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module logic_writeback_tb(
+
+    );
+endmodule
