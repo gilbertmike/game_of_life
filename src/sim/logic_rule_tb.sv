0e00ce0c916ed734db8cdc92630eb8447b37d1b4
Michael Gilbert <gilbertm@mit.edu>
2021-11-13 16:15:51 -0500

    Add test benches for game logic

---
+++ src/sim/logic_rule_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/13/2021 04:13:56 PM
+// Design Name: 
+// Module Name: logic_rule_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module logic_rule_tb(
+
+    );
+endmodule
