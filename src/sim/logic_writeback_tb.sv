7f1ebf2ef37b27e36ca56d4209a3e4bdd57af4dc
Michael Gilbert <gilbertm@mit.edu>
2021-11-15 10:36:58 -0500

    Test writeback

--- src/sim/logic_writeback_tb.sv
+++ src/sim/logic_writeback_tb.sv
@@ -1,26 +1,56 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/13/2021 04:14:21 PM
-// Design Name: 
-// Module Name: logic_writeback_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+`default_nettype none
+module logic_writeback_tb;
+    logic clk;
+    always #5 clk = !clk;
 
-module logic_writeback_tb(
+    // Input
+    logic stall, start;
+    logic[NUM_PE-1:0] next_state;
 
-    );
+    // Output
+    logic wr_en;
+    addr_t addr_w;
+    data_t data_w;
+
+    logic_writeback wb(.clk_in(clk), .stall_in(stall), .start_in(start),
+                       .next_state_in(next_state), .wr_en_out(wr_en),
+                       .addr_w_out(addr_w), .data_w_out(data_w));
+
+    initial begin
+        clk = 0;
+        start = 0;
+        stall = 0;
+        next_state = 1'b0;
+
+        // Start test
+        #20;
+        start = 1;
+        next_state = 1'b1;
+        #10;
+        start = 0;
+        next_state = 1'b1;
+        for (integer i = 0; i < WORD_SIZE; i++) begin
+            #10;
+            next_state = !next_state;
+        end
+
+        // Test stall
+        #10;
+        stall = 1;
+        next_state = 1'b0;
+        #10;
+        stall = 1;
+        next_state = 1'b1;
+        #10;
+        stall = 0;
+        next_state = 1'b1;
+        for (integer i = 0; i < WORD_SIZE; i++) begin
+            #10;
+            next_state = !next_state;
+        end
+    end
 endmodule
+`default_nettype wire
+
