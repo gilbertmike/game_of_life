7d35082d75f3e8e1ebbe9baae4fb1bb4885647cf
FionaLin13 <59299503+FionaLin13@users.noreply.github.com>
2021-12-07 22:43:02 -0500

    generating coe file for audio

---
+++ scripts/wav_to_coe.py
@@ -0,0 +1,20 @@
+COE_HEADER = '''memory_initialization_radix=8;
+memory_initialization_vector=
+'''
+
+def wav_to_coe(fname):
+    coe = COE_HEADER
+    with open(fname, 'rb') as f:
+        bytes = f.read()
+    for byte in bytes:
+        byte = bin(byte).strip('0b')
+        byte = (8-len(byte))*'0' + byte
+        coe += byte + ',\n'
+    return coe
+
+if __name__ == '__main__':
+    import sys
+    fname = sys.argv[1]
+    coe_fname = sys.argv[2]
+    with open(coe_fname, 'w') as f:
+        f.write(wav_to_coe(fname))
